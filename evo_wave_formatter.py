f2101716b631990ec750e30cc461f20ad3252c89
EliakinCosta <eliakim170@gmail.com>
2017-04-05 21:44:48 -0300

    Comeco da implementacao de Sectors e Molecules

--- evo_wave_formatter.py
+++ evo_wave_formatter.py
@@ -1,1 +1,24 @@
 import repository_facade
+
+
+class Sector(object):
+    
+    def __init__(self):
+        pass
+    
+    def molecules(self, sector_name):
+        pass
+    
+    
+
+class Molecule(object):
+    """"""
+    def __init__(self, color='FFFF0000'):
+        self.data = {}
+        self.window_position = 0
+        self.color = color
+    
+    def get_data(self):
+        """ It's a method to format data to the molecule """
+        pass
+    
