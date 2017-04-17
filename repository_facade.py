7d95e376573d9586b56aeae48f97c0c22b79ed51
EliakinCosta <eliakim170@gmail.com>
2017-04-16 23:06:48 -0300

    Changes to load from just one json

--- repository_facade.py
+++ repository_facade.py
@@ -5,6 +5,7 @@ import json
 import time
 import os
 import list_windows
+import requests
 
 
 class RepositoryFacade(object):
@@ -20,13 +21,9 @@ class RepositoryFacade(object):
         self._load_contributors()
 
     def _load_commits(self):
-        files = os.listdir('./files')
-        files.sort()
-        for _file in files:
-            if _file.split('.')[1]=='json':
-                with open('./files/{0}'.format(_file), 'r') as evowave_file:
-                    commits = json.loads(evowave_file.read())
-                    self.commits.extend([commit for commit in commits])
+        with open('./new_data.json', 'r') as evowave_file:
+            commits = json.loads(evowave_file.read())
+            self.commits.extend([commit for commit in commits])
 
     def _load_contributors(self):
         contributors = []
@@ -42,8 +39,8 @@ class RepositoryFacade(object):
     def _commit_window(self, commit):
         for window in list_windows.list_windows():
             if(date_parser.parse(commit['commit']['committer']['date'])>=window[1] and
-               date_parser.parse(commit['commit']['committer']['date'])<=window[2]):
-               return window[0]
+            date_parser.parse(commit['commit']['committer']['date'])<=window[2]):
+                return window[0]
 
 
     def commits_by_window(self, start_period, end_pediod, contributor):
@@ -51,8 +48,29 @@ class RepositoryFacade(object):
                                                       date_parser.parse(commit['commit']['committer']['date'])<=end_pediod and
                                                       commit['commit']['author']['email']==contributor)]
 
+
 if __name__=='__main__':
-    #18 2016-2017 NAO foi executado ainda
-    git_wrapper = RepositoryFacade('sandroandrade', 'emile-server')
-    print(git_wrapper.contributors_windows)
-    print(len(git_wrapper.contributors_windows))
+    git_wrapper = RepositoryFacade('KDE', 'krita')
+    _commits = []
+    _contributors = []
+    print(len(git_wrapper.commits))
+    count = 0
+
+    with open('contributors.json', 'r') as data:
+        _contributors = json.load(data)['contributors']
+        _contributors = [contrib['email'] for contrib in _contributors]
+
+    with open('commits_details_43000_44000.json', 'w') as dest_file:
+        for commit in git_wrapper.commits[43000:44000]:
+            if commit['commit']['author']['email'] in _contributors:
+                url = 'https://api.github.com/repositories/43975673/commits/{0}'.format(commit['sha'])
+                headers = {'Authorization': 'token {0}'.format('387292809a22b21fbb1661246aa1d340d8700e87'), "Content-type" : "application/json"}
+                result = requests.get(url, headers=headers)
+                count += 1
+                print(result, count)
+                if result.status_code==200:
+                    commit_details = result.json()
+                    _commits.append(dict(sha=commit_details.get('sha'), stats=commit_details.get('stats'), files=commit_details.get('files')))
+                else:
+                    break
+        dest_file.write(json.dumps(_commits))
