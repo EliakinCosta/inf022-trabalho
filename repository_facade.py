663e7dca334464279a4b5b2c1c0467ed95a3084b
EliakinCosta <eliakim170@gmail.com>
2017-05-10 16:46:04 -0300

    Changed to get size from sectors

--- repository_facade.py
+++ repository_facade.py
@@ -63,21 +63,10 @@ if __name__=='__main__':
     print(len(git_wrapper.commits))
     count = 0
 
-    with open('contributors.json', 'r') as data:
-        _contributors = json.load(data)['contributors']
-        _contributors = [contrib['email'] for contrib in _contributors]
+    for contributor in git_wrapper.contributors:
+        commits = git_wrapper.commits_by_window(datetime.datetime(2009, 1, 1, tzinfo=pytz.timezone('America/Bahia')),
+                                      datetime.datetime(2010, 1, 1, tzinfo=pytz.timezone('America/Bahia')),
+                                      datetime.datetime(1980, 9, 7, tzinfo=pytz.timezone('America/Bahia')), datetime.datetime(1981, 9, 7, tzinfo=pytz.timezone('America/Bahia')), contributor['email'])
+        count += len(commits)
 
-    with open('commits_details_43000_44000.json', 'w') as dest_file:
-        for commit in git_wrapper.commits[43000:44000]:
-            if commit['commit']['author']['email'] in _contributors:
-                url = 'https://api.github.com/repositories/43975673/commits/{0}'.format(commit['sha'])
-                headers = {'Authorization': 'token {0}'.format('387292809a22b21fbb1661246aa1d340d8700e87'), "Content-type" : "application/json"}
-                result = requests.get(url, headers=headers)
-                count += 1
-                print(result, count)
-                if result.status_code==200:
-                    commit_details = result.json()
-                    _commits.append(dict(sha=commit_details.get('sha'), stats=commit_details.get('stats'), files=commit_details.get('files')))
-                else:
-                    break
-        dest_file.write(json.dumps(_commits))
+    print(count)
